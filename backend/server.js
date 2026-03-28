a5631293960bd5f3d06743344a1200be28aae4a4
Mehak Rauf <mehak.fatima2386@gmail.com>
2026-03-28 13:21:14 +0500

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = 8080;
+app.listen(PORT, () => { console.log("App is listening!"); });
